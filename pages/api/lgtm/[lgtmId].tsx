84ecca19a3a49821fcfdf0002c533e8be20f15f3
tsujioka taichi <t.tsujioka@jxpress.net>
2022-10-12 20:30:48 +0900

    修正だ

--- pages/api/lgtm/[lgtmId].tsx
+++ pages/api/lgtm/[lgtmId].tsx
@@ -3,18 +3,24 @@ import puppeteer from "puppeteer";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const lgtmId = req.query.lgtmId;
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: puppeteer.executablePath(),
-    ignoreDefaultArgs: ["--disable-extensions"],
-  });
-  const page = await browser.newPage();
-  await page.goto(`${process.env.CLIENT_BASE_URL}/lgtm/${lgtmId}`, {
+  let browser: puppeteer.Browser | undefined = undefined;
+  try {
+    browser = await puppeteer.launch({
+      headless: true,
+      executablePath: puppeteer.executablePath(),
+      ignoreDefaultArgs: ["--disable-extensions"],
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
+  const page = await browser?.newPage();
+  await page?.goto(`${process.env.CLIENT_BASE_URL}/lgtm/${lgtmId}`, {
     waitUntil: "load",
   });
-  const selector = await page.$("#target");
+  const selector = await page?.$("#target");
   const buffer = await selector?.screenshot({ path: "page-ss.png" });
-  await browser.close();
+  await browser?.close();
   res.setHeader("Content-Type", "image/png");
   res.setHeader(
     "Cache-Control",
