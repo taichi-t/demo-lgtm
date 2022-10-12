a9530229d7203c5da74834216bdc03aa4d521d4b
tsujioka taichi <t.tsujioka@jxpress.net>
2022-10-12 21:17:15 +0900

    テst

--- pages/api/healthcheck.ts
+++ pages/api/healthcheck.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200);
+  res.status(200).json({});
 };
 
 export default handler;
